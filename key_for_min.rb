61f0871fccae8fa036e48cd20b05c3acfe4cd77b
angelica winkler <caw400@nyu.edu>
2018-08-05 16:55:51 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  valuesArray = []
+
+  name_hash.collect do |name, value|
+    valuesArray.push(value)
+  end
+
+  index = valuesArray.min
+
+  name_hash.keys[index]
+
 end
